a1d342e7c86d13cfcea603bc2af2406315cfc7ed
Arthur Sore <arthur.sore@gmail.com>
2013-01-13 23:38:38 +0100

    Arc, Tir de fleche, physique
    Code pour l'arc qui instancie la fleche dans la bonne position puis
    lorsque le bouton est relaché la détache et y applique une force dans la
    bonne direction puis la fleche se plante lorsqu'elle termine son vol.

--- Assets/Twur/Scripts/FlecheEnVol.cs
+++ Assets/Twur/Scripts/FlecheEnVol.cs
@@ -4,14 +4,48 @@ using System.Collections;
 public class FlecheEnVol : MonoBehaviour {
 	
 	public float _tempsDestructionFleche;
+	//le transform au moment du tir
+	private Vector3 vectorTir;
+	
+	private bool FlecheTiree;
 	
 	// Use this for initialization
 	void Start () {
 		
+		
+		FlecheTiree = false;
+		vectorTir = transform.position;
 		Destroy(gameObject, _tempsDestructionFleche);
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if(FlecheTiree)
+		{
+			//la fleche "regarde" dans la bonne direction (pointe devant) en vol.
+			transform.LookAt(vectorTir - rigidbody.velocity);
+		}
+		else if (!FlecheTiree)
+		{
+			
+		}
+		else
+		{
+			Debug.LogError("probleme variable fleche tiree");
+			return;
+		}
+	}
+	
+	void OnCollisionEnter(Collision Col) {
+  		rigidbody.isKinematic =true; // stop physics
+ 		//transform.parent = Col.transform; // doesn't move yet, but will move w/what it hit
+	}
+	
+	public void EstTiree(bool tir) {
+		FlecheTiree = tir;
 	}
 }
+
--- Assets/Twur/Scripts/TirFleches.cs
+++ Assets/Twur/Scripts/TirFleches.cs
@@ -7,11 +7,15 @@ public class TirFleches : MonoBehaviour {
 	public Transform cameraJoueurTransform;
 	public float forceTir;
 	
+	private Rigidbody instanceFleche;
+	private bool _flecheChargee;
+	
 
 	
 	
 	// Use this for initialization
 	void Start () {
+		_flecheChargee = false;
 
 		
 	}
@@ -20,12 +24,42 @@ public class TirFleches : MonoBehaviour {
 	void Update () {
 		if(Input.GetButtonDown("Fire1"))
 		{
-			//trouver comment régler ça, ajouter +90 provoque un bug lors de la rotation du transform et la fleche ne part pas toujours dans le meme sens, sans y toucher, elle part de profil.
-			float tempRot = transform.rotation.z ;
-			
-			Rigidbody instanceFleche = (Rigidbody) Instantiate(prefabFleche, transform.position, Quaternion.Euler(transform.rotation.x, transform.rotation.y , tempRot));
-			
-			instanceFleche.AddForce(transform.forward * forceTir);
+			if(_flecheChargee)
+			{
+				return;
+			}
+			else if(!_flecheChargee)
+			{
+				instanceFleche = (Rigidbody) Instantiate(prefabFleche, transform.position, transform.rotation);
+				// instantie comme enfant du transform:
+				instanceFleche.transform.parent = transform;
+				
+				// je boque la physique de la fleche pour pas qu'elle tombe au sol avant d'être tirée
+				instanceFleche.isKinematic = true;
+				_flecheChargee = true;
+			}
+		}
+		
+		if(Input.GetButtonUp("Fire1"))
+		{
+			if(_flecheChargee){
+				//si la fleche est chargée et que le bouton est relachée, on remet la physique et on applique une force.
+				instanceFleche.isKinematic = false;
+				instanceFleche.AddForce(transform.forward * forceTir);
+				
+				
+				//on se détache du parent Arc pour ne pas être influé par son déplacement lors du vol
+				instanceFleche.transform.parent = null;
+				_flecheChargee = false;
+				
+				//acces a la fonction publique de fleche en vol via getComponent
+				FlecheEnVol scriptFleche = instanceFleche.GetComponent<FlecheEnVol>();
+        		scriptFleche.EstTiree(true);
+			}
+			else if (!_flecheChargee){
+				return;
+			}
+	
 		}
 	}
 }
